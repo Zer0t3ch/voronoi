140d707b1c9fe072038c4ec2987af8146e6a7475
Zachary Hill <zer0t3ch@live.com>
2014-12-16 01:47:23 -0600

    Completely redone. Ready for convex hull algorithm

--- voronoi.py
+++ voronoi.py
@@ -4,77 +4,166 @@ import sys
 import os
 
 
-class point:
+class Point:
 	def __init__(self, x=0, y=0):
 		self.loc = [x, y]
 	loc = [0, 0]
 
 
-def random_points(count, xmax=100, ymax=100, xmin=0, ymin=0):
-	pts = []
-	for i in xrange(count):
-		ptx = random.randint(xmin, xmax)
-		pty = random.randint(ymin, ymax)
-		pts.append(point(ptx, pty))
-	return pts
+class Line:
+	def __init__(self, x1, y1, x2, y2):
+		self.p1 = Point(x1, y1)
+		self.p2 = Point(x2, y2)
+
+	def get_points(self):
+		return self.p1, self.p2
+
+	def get_locations(self):
+		return self.p1.loc, self.p2.loc
+
+	def get_raw(self):
+		return [
+			self.p1.loc[0],
+			self.p1.loc[1],
+			self.p2.loc[0],
+			self.p2.loc[1]
+		]
 
 
-def draw_point(canvas, x, y):
-	a = x * 3
-	b = y * 3
-	canvas.create_line(
-		a - 1, b - 1,
-		a - 1, b + 1
-	)
-	canvas.create_line(
-		a - 1, b + 1,
-		a + 1, b + 1
-	)
-	canvas.create_line(
-		a + 1, b + 1,
-		a + 1, b - 1
-	)
-	canvas.create_line(
-		a + 1, b - 1,
-		a - 1, b - 1
-	)
+class Application(Frame):
+	size = [1200, 900]
+	csize = [900, 900]
+	cscale = 9
+	frames = ['', '']
+	buttons = {}
+	point_count = 50
+	points = []
+	lines = {
+		'a': [],
+		'c': [],
+		'd': [],
+		'v': []
+	}
+	toggles = {
+		'c': False,
+		'd': False,
+		'v': False
+	}
 
+	def loc(self, p):
+		x, y = p.loc
+		a = x * self.cscale
+		b = y * self.cscale
+		return a, b
 
-def draw_line(canvas, p1, p2):
-	x1 = p1.loc[0] * 3
-	y1 = p1.loc[1] * 3
-	x2 = p2.loc[0] * 3
-	y2 = p2.loc[1] * 3
-	canvas.create_line(x1, y1, x2, y2)
-
-
-def paint():
-	c.delete(ALL)
-	points = random_points(50)
-	for p in points:
-		draw_point(
-			c,
-			p.loc[0], p.loc[1]
+	def create_frames(self):
+		self.frames[0] = Frame(
+			self,
+			width=900,
+			height=900
 		)
+		self.frames[1] = Frame(
+			self,
+			width=300,
+			height=900
+		)
+		self.frames[0].pack(side='left')
+		self.frames[1].pack(side='right', fill='y')
+		self.c = c = Canvas(self.frames[0], width=900, height=900)
+		c.pack()
+
+	def clear_points(self):
+		self.points = []
+
+	def generate_points(self):
+		for i in xrange(self.point_count):
+			randx = random.randint(0, self.csize[0] / self.cscale)
+			randy = random.randint(0, self.csize[1] / self.cscale)
+			p = Point(randx, randy)
+			self.points.append(p)
+
+	def draw_points(self):
+		for p in self.points:
+			a, b = self.loc(p)
+			self.c.create_line(
+				a - 1, b - 1,
+				a + 1, b + 1
+			)
+			self.c.create_line(
+				a + 1, b - 1,
+				a - 1, b + 1
+			)
+
+	def regenerate_points(self):
+		self.clear_points()
+		self.c.delete(ALL)
+		self.generate_points()
+		self.draw_points()
+
+	def clear_lines(self, cat='a'):
+		for l in self.lines[cat]:
+			self.c.delete(l)
+
+	def add_line(self, line, cat='a'):
+		lines['a'].append(line)
+		if cat != 'a':
+			lines[cat].append(line)
+
+	def toggle_mode(self, mode):
+		if self.toggles[mode]:
+			self.toggles[mode] = False
+		else:
+			self.toggles[mode] = True
+
+	def toggle_convex_hull(self):
+		if self.toggles['c']:
+			self.clear_lines(cat='c')
+			self.toggle_mode('c')
+		else:
+			# Calculate lines
+			# Add lines to array and draw
+
+	def create_buttons(self, parent):
+		quit = self.buttons['quit'] = Button(
+			parent,
+			text='QUIT',
+			command=self.quit,
+			fg='red'
+		)
+		regen_points = self.buttons['regen_points'] = Button(
+			parent,
+			text='Regenerate Points',
+			command=self.regenerate_points
+		)
+		clear_lines = self.buttons['clear_lines'] = Button(
+			parent,
+			text='Clear Lines',
+			command=self.clear_lines
+		)
+		toggle_convex = self.buttons['toggle_convex'] = Button(
+			parent,
+			text='Toggle Convex Hull',
+			command=self.toggle_convex_hull
+		)
+		quit.pack(fill='x', side='bottom')
+		toggle_convex.pack(fill='x')
+		regen_points.pack(fill='x')
+		clear_lines.pack(fill='x')
+
+	def __init__(self, master):
+		Frame.__init__(
+			self, master,
+			width=self.size[0],
+			height=self.size[1]
+		)
+		self.pack()
+		self.create_frames()
+		self.create_buttons(self.frames[1])
 
 
-points = []
-
-w = Tk()
-fc = Frame(w, width=300, height=300)
-fo = Frame(w, width=200, height=300)
-fc.pack({'side': 'left'})
-fo.pack({'side': 'right'})
-c = Canvas(fc, width=300, height=300)
-c.pack()
-
-qb = Button(fo)
-qb['text'] = 'QUIT'
-qb['command'] = w.quit
-qb['fg'] = 'red'
-qb.pack({'side': 'bottom'})
-
-paint()
-
-w.mainloop()
-w.destroy()
+window = Tk()
+app = Application(window)
+app.generate_points()
+app.draw_points()
+app.mainloop()
+window.destroy()
