0f3395c826694398a5120d8e909d502f90804c37
Zachary Hill <zer0t3ch97@gmail.com>
2014-12-19 12:15:49 -0600

    Switched to a config-file-based setup; improved resizing; points now regen on resize; hull now gets drawn on point regen

---
+++ config.py
@@ -0,0 +1,3 @@
+window_size = [1200, 600]
+
+point_count = 15
--- main.py
+++ main.py
@@ -4,12 +4,11 @@ from Tkinter import *
 from geo import *
 import random
 
-POINT_COUNT = 50
-
 
 class Application(Frame):
-	f_size = [1200, 900]
-	canvas_size = [900, 900]
+	point_count = 5
+	window_size = [300, 150]
+	canvas_size = [200, 150]
 	canvas_scale = 1
 	dot_scale = 1
 	frames = ['', '']
@@ -34,26 +33,35 @@ class Application(Frame):
 		return a, b
 
 	def create_frames(self):
-		self.frames[0] = Frame(
+		self.canvas_size[0] = self.window_size[0] - 100
+		self.canvas_size[1] = self.window_size[1]
+		frame_one = self.frames[0] = Frame(
 			self,
-			width=900,
-			height=900
+			width=self.canvas_size[0],
+			height=self.canvas_size[1]
 		)
-		self.frames[1] = Frame(
+		frame_two = self.frames[1] = Frame(
 			self,
-			width=300,
-			height=900
+			width=100,
+			height=self.window_size[1]
 		)
-		self.frames[0].pack(side='left')
-		self.frames[1].pack(side='right', fill='y')
-		self.canvas = c = Canvas(self.frames[0], width=900, height=900)
-		c.pack()
+
+		frame_two.pack(side='right', fill='y')
+		frame_one.pack(side='left', fill='both')
+		self.canvas = Canvas(self.frames[0], width=900, height=900)
+		self.canvas.bind('<Configure>', self.on_resize)
+		self.canvas.pack()
+
+	def on_resize(self, event):
+		self.canvas_size = event.width, event.height
+		self.regenerate_points()
+		print self.canvas_size
 
 	def clear_points(self):
 		self.points = []
 
 	def generate_points(self):
-		for i in xrange(POINT_COUNT):
+		for i in xrange(self.point_count):
 			randx = random.randint(0, self.canvas_size[0] / self.canvas_scale)
 			randy = random.randint(0, self.canvas_size[1] / self.canvas_scale)
 			p = Point(randx, randy)
@@ -76,6 +84,9 @@ class Application(Frame):
 		self.canvas.delete(ALL)
 		self.generate_points()
 		self.draw_points()
+		if self.toggles['c']:
+			self.toggle_convex_hull()
+		self.toggle_convex_hull()
 
 	def clear_lines(self, cat='a'):
 		for l in self.lines[cat]:
@@ -136,10 +147,16 @@ class Application(Frame):
 		clear_lines.pack(fill='x')
 
 	def __init__(self, master):
+		with open('config.py', 'r') as conf_file:
+			conf = {}
+			exec(conf_file.read(), conf)
+			conf_file.close()
+			self.point_count = conf['point_count']
+			self.window_size = conf['window_size']
 		Frame.__init__(
 			self, master,
-			width=self.f_size[0],
-			height=self.f_size[1]
+			width=self.window_size[0],
+			height=self.window_size[1]
 		)
 		self.pack()
 		self.create_frames()
@@ -150,5 +167,6 @@ window = Tk()
 app = Application(window)
 app.generate_points()
 app.draw_points()
+window.focus_set()
 app.mainloop()
 window.destroy()
